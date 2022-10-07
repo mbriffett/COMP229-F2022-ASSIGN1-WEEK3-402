b585c67d6738f69b46e684972fba2428b733a860
mbriffett <matthewbriffett@gmail.com>
2022-10-07 18:16:46 -0230

    Added horizontal file structure and modules like ejs and morgan

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-// Third party modules
-import Express from "express";
+// Third party modules 
+import express from "express";
+//middleware to extend express
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import session from "express-session";
 
+// ES Modules fix for __dirname 
+import path, {dirname} from 'path'; 
+import { fileURLToPath } from 'url'; 
+const __dirname = dirname(fileURLToPath(import.meta.url)); //directory name
+
 // instantiate app-server
-const app = Express();
+const app = express();
+
+//setup ViewEngine EJS - previously installed (all modules in node_modules)
+app.set('views', path.join(__dirname, '/views')); //views in [directoryname]/views
+app.set('view engine', 'ejs'); //tells express which view engine to use
+
+app.use(logger('dev')); //logger (morgan) used in dev environment
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); //ensures no environment specific encoding causes errors with express
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public'))); //serves static files (files on host machine to be used/executed there)
+app.use(session({
+    secret: "BigSecret", //for encrypting session values 
+    saveUninitialized: "false", //these lines mean session is lost on reload
+    resave: "false"
+}))
 
 // custom middleware
 function helloWorld(req, res, next){
