522fa64f2f18f05440bc66d338206dcfeab5cb51
mbriffett <matthewbriffett@gmail.com>
2022-10-08 20:55:02 -0230

    Major Changes. Forgot to commit

--- server.js
+++ server.js
@@ -1,51 +1,60 @@
-// Third party modules 
-import express from "express";
-//middleware to extend express
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import session from "express-session";
+//Matthew Briffett 301247484 Centennial College COMP229 Fall 2022
 
-// ES Modules fix for __dirname 
-import path, {dirname} from 'path'; 
-import { fileURLToPath } from 'url'; 
-const __dirname = dirname(fileURLToPath(import.meta.url)); //directory name
+import debug from 'debug';
+debug('comp-229');
+import http from 'http';
 
-// instantiate app-server
-const app = express();
+import app from './app/app.js';
 
-//setup ViewEngine EJS - previously installed (all modules in node_modules)
-app.set('views', path.join(__dirname, '/views')); //views in [directoryname]/views
-app.set('view engine', 'ejs'); //tells express which view engine to use
+const PORT = normalizePort(process.env.PORT || 3000); //server defined port or 3000
+app.set('port', PORT);
 
-app.use(logger('dev')); //logger (morgan) used in dev environment
-app.use(express.json());
-app.use(express.urlencoded({extended: false})); //ensures no environment specific encoding causes errors with express
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public'))); //serves static files (files on host machine to be used/executed there)
-app.use(session({
-    secret: "BigSecret", //for encrypting session values 
-    saveUninitialized: "false", //these lines mean session is lost on reload
-    resave: "false"
-}))
+const server = http.createServer(app); //create web server using express application (express module working with http module here)
 
-// custom middleware
-function helloWorld(req, res, next){
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World');
-};
+server.listen(PORT);
+server.on('error', onError); //call these functions on error or listening respectively
+server.on('listening', onListening);
 
-// custom middleware
-function byeWorld(req, res, next){
-    res.setHeader('Content-Type','text/plain');
-    res.end('Good Bye World');
-};
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
 
-// add middleware to express application
-app.use('/hello',helloWorld);
-app.use('/bye', byeWorld);
-app.use('/', helloWorld);
+    if (port >= 0) {
+        return port;
+    }
 
-// run app
-app.listen(3000);
+    return false;
+}
 
-console.log('Server running at http://localhost:3000');
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() 
+{
+  let addr = server.address();
+  let bind = 'pipe ' + addr;
+  debug('Listening on ' + bind);  
+}
